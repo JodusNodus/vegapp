5bfc0e08e09f0606a3776e928d76f3c0569924e9
Thomas Billiet <billiet.thomas@outlook.com>
2018-05-03 20:09:23 +0200

    Add cors support

--- app/src/main/java/be/tabtabstudio/veganapp/data/network/ApiService.java
+++ app/src/main/java/be/tabtabstudio/veganapp/data/network/ApiService.java
@@ -19,43 +19,56 @@ import retrofit2.Call;
 import retrofit2.http.*;
 
 public interface ApiService {
+    @Headers("Origin: https://thomasbilliet.com")
     @POST("signup")
     Call<LoginResult> signup(@Body UserSignupBody userSignupBody);
 
+    @Headers("Origin: https://thomasbilliet.com")
     @POST("login")
     Call<LoginResult> login(@Body UserLoginBody userLoginBody);
 
+    @Headers("Origin: https://thomasbilliet.com")
     @POST("logout")
     Call<Void> logout();
 
+    @Headers("Origin: https://thomasbilliet.com")
     @POST("api/location")
     Call<Void> setLocation(@Body Location location);
 
+    @Headers("Origin: https://thomasbilliet.com")
     @GET("api/products")
     Call<GetProductsResult> getProducts(@Query("searchquery") String searchquery, @Query("orderby") String orderby, @Query("size") int size, @Query("page") int page, @Query("labels") String labels);
 
+    @Headers("Origin: https://thomasbilliet.com")
     @GET("api/products/{ean}")
     Call<GetProductResult> getProduct(@Path("ean") long ean);
 
+    @Headers("Origin: https://thomasbilliet.com")
     @POST("api/products/{ean}/rate")
     Call<Void> rateProduct(@Path("ean") long ean, @Body RateProductBody rateProductBody);
 
+    @Headers("Origin: https://thomasbilliet.com")
     @DELETE("api/products/{ean}")
     Call<Void> markProductInvalid(@Path("ean") long ean);
 
+    @Headers("Origin: https://thomasbilliet.com")
     @GET("api/supermarkets")
     Call<GetSupermarketsResult> getSupermarktes();
 
+    @Headers("Origin: https://thomasbilliet.com")
     @Multipart
     @POST("api/products/picture")
     Call<UploadProductImageResult> uploadProductImage(@Part MultipartBody.Part ean, @Part MultipartBody.Part picture);
 
+    @Headers("Origin: https://thomasbilliet.com")
     @POST("api/products")
     Call<Void> createProduct(@Body CreateProductBody createProductBody);
 
+    @Headers("Origin: https://thomasbilliet.com")
     @GET("api/brands")
     Call<GetBrandsResult> getBrands();
 
+    @Headers("Origin: https://thomasbilliet.com")
     @GET("api/labels")
     Call<GetLabelsResult> getLabels();
 }
